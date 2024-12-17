296abdb758e51f82b9387f2ffa3d066aaaa5f275
Veniamin <benjaminshapkin@gmail.com>
2024-12-17 18:58:57 +0200

    first commit8week

---
+++ Week8_EpicGame/Program.cs
@@ -0,0 +1,33 @@
+﻿
+string folderPath = @"C:\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+
+
+//string[] heroes = {"Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo"};
+//string[] villains = {"Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron"};
+
+
+string[] weapon = {"magic wand", "plastic fork", "banana", "wooden sword", "Lego brick"};
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
